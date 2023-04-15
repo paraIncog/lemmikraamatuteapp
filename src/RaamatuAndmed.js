5090867f4d37986f6b5f5225b91d806d21ccf558
paraIncog <selgeandreas02@gmail.com>
2023-04-15 15:32:32 +0300

    Add second book

--- src/RaamatuAndmed.js
+++ src/RaamatuAndmed.js
@@ -1,8 +1,25 @@
 import Raamat from "./Raamat"
 
 import g_orwell_pic from './book_pics/g_orwell_pic.jpg';
+import a_exupery_pic from './book_pics/a_exupery_pic.jpg';
 
 const raamat1 = {
+    Pealkiri: "1984",
+    Autor: "George Orwell",
+    Avalikustatud: 1949,
+    Kirjeldus: "Winston Smith töötab Okeaania Tõeministeeriumis, kus tema ülesanne on ajalugu ümber kirjutada. Ta on pisike mutter Partei masinavärgis, mida juhib kõikvõimas Suur Vend. Totalitaarses ühiskonnas, kus valitseb propaganda, järelevalve ja tsensuur, leiab Winston kolleeg Julia seltskonnast tuge - armastuse, mõningase vabaduse ja lootuse paremaks tulevikuks. Kuid nad ei tea, et Suur Vend jälgib kõike ja kõiki.",
+    RPilt: g_orwell_pic
+}
+
+const raamat2 = {
+    Pealkiri: "Väike Prints",
+    Autor: "Antoine de Saint-Exupery",
+    Avalikustatud: 1943,
+    Kirjeldus: "1943. aastal ilmunud „Väikest printsi”, ühtaegu lihtsat ja sügavamõttelist lasteraamatut, mis arutleb üksinduse, sõpruse, armastuse ja kaotuse üle, on avaldatud sadades keeltes ja murretes ning müüdud kümneid miljoneid eksemplare. Seda peetakse tõlgituimaks ja loetuimaks prantsuse raamatuks ning prantslased hääletasid selle ka 20. sajandi parimaks prantsuse raamatuks. Käesolev väljaanne on „Väikese printsi” algselt 2017 ilmunud uus tõlge.",
+    RPilt: a_exupery_pic
+}
+
+const raamat3 = {
     Pealkiri: "1984",
     Autor: "George Orwell",
     Avalikustatud: 1949,
@@ -21,7 +38,13 @@ function RaamatuAndmed() {
                 Kirjeldus={raamat1.Kirjeldus}
                 RPilt={raamat1.RPilt}
             />
-            <Raamat />
+            <Raamat
+                Pealkiri={raamat2.Pealkiri}
+                Autor={raamat2.Autor}
+                Avalikustatud={raamat2.Avalikustatud}
+                Kirjeldus={raamat2.Kirjeldus}
+                RPilt={raamat2.RPilt}
+            />
             <Raamat />
         </div>
     )
